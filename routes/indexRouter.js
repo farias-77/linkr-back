b8fa3a434b983d9c5d0776f339b3eefa952e06be
farias-77 <gabrielfalves2003@gmail.com>
2022-08-09 19:18:57 -0300

    add router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 
 import authRouter from "./authRouter.js";
+import usersRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
-
+router.use(usersRouter);
 
 
 
