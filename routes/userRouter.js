bde68ae110dbc2b96bf1f184cae141496379b165
farias-77 <gabrielfalves2003@gmail.com>
2022-08-10 19:45:01 -0300

    feat: add token middleware to user routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import { getUsers, getUserPosts } from "../controllers/userController.js";
+import { tokenMiddleware } from "../middlewares/tokenMiddleware";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/users/:searchInput", getUsers);
-router.get("/user/:id", getUserPosts);
+router.get("/users/:searchInput", tokenMiddleware, getUsers);
+router.get("/user/:id", tokenMiddleware, getUserPosts);
 
 export default router;
