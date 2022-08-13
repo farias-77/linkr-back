2ea0533325c4253a8654900c260f903baa4d138e
farias-77 <gabrielfalves2003@gmail.com>
2022-08-13 15:40:11 -0300

    bug fix

--- middlewares/metadataMiddleware.js
+++ middlewares/metadataMiddleware.js
@@ -7,17 +7,15 @@ export async function metadataMiddleware(req, res){
         const postId = res.locals.postId;
 
         urlMetadata(`${url}`).then(
-            async function (metadata) { // success handler
-                try{
-                    console.log(metadata)
-                    await connection.query(`
-                        INSERT INTO metadata ("postId", title, image, description)
-                        VALUES ($1, $2, $3, $4)
-                    `,[postId, metadata.title, metadata.image, metadata.description]);
-                }catch(error){
-                    return res.status(500).send(error.message);
-                }
+            function (metadata) { // success handler
+                console.log(metadata)
+                
+                await connection.query(`
+                    INSERT INTO metadata ("postId", title, image, description)
+                    VALUES ($1, $2, $3, $4)
+                `,[postId, metadata.title, metadata.image, metadata.description]);
             },
+
             function (error) { // failure handler
                 console.log(error)
             });
