f83d20e27d24deaa2587143c518a9d244479a240
farias-77 <gabrielfalves2003@gmail.com>
2022-08-13 20:49:48 -0300

    feat: delete post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -30,3 +30,29 @@ export async function registerPost(req, res) {
         return res.status(500).send(error.message);
     }
 }
+
+export async function deletePost(req, res){
+    try{
+        const postId = req.params.postId;
+        const id = res.locals.id;
+
+        const { rows: post } = await connection.query(`
+            SELECT * 
+            FROM posts
+            WHERE id = $1;
+        `, [postId]);
+
+        if(post[0].userId !== id){
+            return res.status(401).send("Você não tem autorização para excluir esse post!");
+        }
+
+        await connection.query(`
+            DELETE FROM posts
+            WHERE id = $1;
+        `, [postId]);
+
+        return res.sendStatus(200);
+    }catch(error){
+        return res.status(500).send(error.message);
+    }
+}
