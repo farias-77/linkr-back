4381a94cde4b23d453943c5dba921f98d1b78585
farias-77 <gabrielfalves2003@gmail.com>
2022-08-16 16:25:50 -0300

    endpoint to get comments by postId

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerPost, deletePost, getTimelinePosts, editPost, postComment } from "../controllers/postsController.js";
+import { registerPost, deletePost, getTimelinePosts, editPost, postComment, getComments } from "../controllers/postsController.js";
 import { registerPostMiddleWare, userValidation, deleteLikes, deleteMetadata, deletePosts_Hashtags, editPostMiddleware } from "../middlewares/postsMiddleware.js";
 import { tokenMiddleware } from "../middlewares/tokenMiddleware.js";
 
@@ -10,5 +10,6 @@ router.post("/register-post", tokenMiddleware, registerPostMiddleWare, registerP
 router.delete("/delete-post/:postId", tokenMiddleware, userValidation, deleteLikes, deleteMetadata, deletePosts_Hashtags, deletePost);
 router.put("/update/:postId",tokenMiddleware,editPostMiddleware, deletePosts_Hashtags, editPost);
 router.post("/comment/:postId", tokenMiddleware, postComment);
+router.get("/comment/:postId", tokenMiddleware, getComments);
 
 export default router;
