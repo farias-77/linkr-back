42ce6b7efc0e94461ae1469fd6551a8d27c9aa29
farias-77 <gabrielfalves2003@gmail.com>
2022-08-18 21:05:44 -0300

    edit original post by editing repost

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { registerPost, deletePost, getTimelinePosts, editPost, postComment, getComments, registerRepost, getRepostCount } from "../controllers/postsController.js";
-import { registerPostMiddleWare, userValidation, deleteLikes, deleteMetadata, deletePosts_Hashtags, deleteComments, editPostMiddleware,deleteRepostsMetadata, deleteReposts } from "../middlewares/postsMiddleware.js";
+import { registerPostMiddleWare, userValidation, deleteLikes, deleteMetadata, deletePosts_Hashtags, deleteComments, editPostMiddleware,deleteRepostsMetadata, deleteReposts, editReposts } from "../middlewares/postsMiddleware.js";
 import { tokenMiddleware } from "../middlewares/tokenMiddleware.js";
 
 const router = Router();
@@ -8,7 +8,7 @@ const router = Router();
 router.get("/timeline/:limit", tokenMiddleware, getTimelinePosts);
 router.post("/register-post", tokenMiddleware, registerPostMiddleWare, registerPost);
 router.delete("/delete-post/:postId", tokenMiddleware, userValidation, deleteLikes, deleteMetadata, deletePosts_Hashtags, deleteComments, deleteRepostsMetadata, deleteReposts, deletePost);
-router.put("/update/:postId",tokenMiddleware,editPostMiddleware, deletePosts_Hashtags, editPost);
+router.put("/update/:postId",tokenMiddleware,editPostMiddleware, deletePosts_Hashtags, editReposts, editPost);
 router.post("/comment/:postId", tokenMiddleware, postComment);
 router.get("/comment/:postId", tokenMiddleware, getComments);
 router.post("/repost/:postId", tokenMiddleware, registerRepost);
