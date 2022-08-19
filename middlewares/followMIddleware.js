e724d6d2097bec39c9a9a9c6414191d903d945f7
carlos <caarlos.albuquerq@gmail.com>
2022-08-19 15:40:05 -0300

    feat: add unfollow function

--- middlewares/followMIddleware.js
+++ middlewares/followMIddleware.js
@@ -27,3 +27,23 @@ export async function followMiddleware(req, res, next) {
        return res.status(500).send(error.message);
     }
 }
+
+export async function unfollowUserMiddleware(req, res, next) {
+    try {
+        const followedId = req.params.id;
+        const myId = res.locals.id;
+
+        const { rowCount } = await connection.query(`
+            SELECT * FROM follows WHERE "followedId" = $1 AND "followerId" = $2
+        `, [followedId, myId]);
+        console.log(rowCount);
+        if (!rowCount) {
+            return res.sendStatus(404);
+        }
+
+        res.locals.followedId = followedId;
+        next();
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,6 @@
-import { getUsers, getUserPosts, getUserInfo, getUserById, followUser } from "../controllers/userController.js";
+import { getUsers, getUserPosts, getUserInfo, getUserById, followUser, unfollowUser } from "../controllers/userController.js";
 import { tokenMiddleware } from "../middlewares/tokenMiddleware.js";
-import { followMiddleware } from "../middlewares/followMIddleware.js";
+import { followMiddleware, unfollowUserMiddleware } from "../middlewares/followMIddleware.js";
 import { Router } from "express";
 
 const router = Router();
@@ -10,5 +10,6 @@ router.get("/user/:id/:limit", tokenMiddleware, getUserPosts);
 router.get("/userInfo", tokenMiddleware, getUserInfo);
 router.post("/follow/:id", tokenMiddleware, followMiddleware, followUser);
 router.get("/user/:id", tokenMiddleware, getUserById);
+router.delete("/unfollow/:id", tokenMiddleware, unfollowUserMiddleware, unfollowUser);
 
 export default router;
