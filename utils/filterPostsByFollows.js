ce9a14ad81fa4dd94566eb68bed602c3a9937ad7
Dimitri <assis.dimitri@engenharia.ufjf.br>
2022-08-19 17:21:20 -0300

    Filtro de posts concluido

--- utils/filterPostsByFollows.js
+++ utils/filterPostsByFollows.js
@@ -4,7 +4,19 @@ export async function getPostsFilteredPerFollows(userId, posts){
     const following = await userRepository.getFollowedByUser(userId);
 
     const followingArray = following.rows.map(convertToArray);
-    return posts.filter(post => followingArray.includes(post.userId) || userId === post.userId);
+    let sendablePosts = [];
+    for(let i=0;i<posts.length;i++){
+        if(posts[i].repostUserId === null){
+            if(userId === posts[i].userId || followingArray.includes(posts[i].userId)){
+                sendablePosts.push(posts[i]);
+            }
+        }else{
+            if(userId === posts[i].repostUserId || followingArray.includes(posts[i].repostUserId)){
+                sendablePosts.push(posts[i]);
+            }
+        }
+    }
+    return sendablePosts;
 }
 
 function convertToArray(objectId){
